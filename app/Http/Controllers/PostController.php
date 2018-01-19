2c2c4ea96818603cfeace3b03c31d2a2c523770b
loyalself <plafhtcc@163.com>
2018-01-19 18:04:01 +0800

    how to use wangEditor upload Images

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,13 +11,14 @@ class PostController extends Controller
     public function index()
     {
         //将文章以创建时间从新到旧排序,get()方法取出的是一个collection集合
-        $posts = Post::orderBy('created_at','desc')->get();
+        $posts = Post::orderBy('created_at','desc')->paginate(6);
         return view('post.index',compact('posts'));
     }
-    //文章详情
-    public function show()
+    //文章详情(show方法里需要绑定模型)
+    public function show(Post $post)
     {
-        return view('post.show');
+        //dd($post);    访问路由,地址栏给上文章对应的id就可以了
+        return view('post.show',compact('post'));
     }
     //创建文章
     public function create()
@@ -27,7 +28,16 @@ class PostController extends Controller
     //创建文章逻辑
     public function store()
     {
-        
+        $this->validate(request(),
+            [
+            'title'=>'required|string|max:100|min:5',
+            'content'=>'required|string|min:10'
+            ]
+            /*['title.required'=>'请输入文章标题']*/);
+
+        /*使用create方法时,要在模型里设置可以赋值的属性,它的结果是一个模型*/
+         Post::create(request(['title','content']));
+         return redirect('/posts');
     }
     //编辑文章
     public function edit()
@@ -43,5 +53,10 @@ class PostController extends Controller
     public function delete()
     {
         
+    }
+
+    public function iamgeUpload()
+    {
+        dd(request()->all());
     }
 }
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //可以赋值的属性
+    //protected $fillable = ['title','content'];
+
+    //不可以注入的字段
+    protected $guarded = [];
 }
